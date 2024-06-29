7a0e3d0e08d73649883d4841ae563e893546ff05
Harish ng <91660937+harishconti@users.noreply.github.com>
2024-06-29 11:42:22 +0530

    base page and simple job page done

--- proj_1/flaskProject/app.py
+++ proj_1/flaskProject/app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+json_path = r'temp/emp_data.json'
+
+
+def json_data(path):
+    with open(path) as data_file:
+        data = json.load(data_file)['jobs']
+    return data
 
 
 @app.route('/')
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", jobs=data)
+
+
+@app.route('/api/jobs')
+def list_jobs():
+    return json_data(json_path)
 
 
 if __name__ == '__main__':
